2a4b2ad056000e392b8133f265e77d411082d4c5
Cesar Zabala <cesarzabala@gmail.com>
2016-03-30 10:07:08 -0500

    Estructura del proyecto

---
+++ bo/ClsPublication.php
@@ -0,0 +1,8 @@
+<?php 
+/** 
+ * Esta clase modela el objeto publicacion y sus interacciones y restricciones
+ */
+Class Publication {
+
+}
+ ?>
---
+++ dao/connectionDB.php
@@ -0,0 +1,5 @@
+<?php 
+/** Aqui se realiza toda la logica de conexion a la BD a MySQL usando PDO
+* y las funciones de interacción con las tablas
+*/
+ ?>
