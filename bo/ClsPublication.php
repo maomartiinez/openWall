3931e823d0675e865aa7016b2190f95bc4c79c02
Daniel Moncada Tabares <info@danielmt57.com>
2016-03-30 20:52:58 -0500

    Agregue atributos a ClsPublication.php

--- bo/ClsPublication.php
+++ bo/ClsPublication.php
@@ -4,5 +4,52 @@
  */
 Class Publication {
 
+    private $idPublication;
+    private $text_content;
+    private $count;
+    private $date;
+    
+    
+    function __construct($idPublication, $text_content, $count, $date) {
+        $this->idPublication = $idPublication;
+        $this->text_content = $text_content;
+        $this->count = $count;
+        $this->date = $date;
+    }
+    
+    function getIdPublication() {
+        return $this->idPublication;
+    }
+
+    function getText_content() {
+        return $this->text_content;
+    }
+
+    function getCount() {
+        return $this->count;
+    }
+
+    function getDate() {
+        return $this->date;
+    }
+
+    function setIdPublication($idPublication) {
+        $this->idPublication = $idPublication;
+    }
+
+    function setText_content($text_content) {
+        $this->text_content = $text_content;
+    }
+
+    function setCount($count) {
+        $this->count = $count;
+    }
+
+    function setDate($date) {
+        $this->date = $date;
+    }
+
+
+
 }
  ?>
