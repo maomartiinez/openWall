ea550bb2d4423661a9463639d1c1749c67d8624e
maomartiinez <maomm25@hotmail.com>
2016-04-01 19:26:38 -0500

    "formato"

--- index.php
+++ index.php
@@ -1,24 +1,24 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="UTF-8">
+    <head>
+        <meta charset="UTF-8">
         <link href="assets/css/styles.css" rel="stylesheet" type="text/css"/>
-	<script src="http://code.jquery.com/jquery-latest.js"></script>
+        <script src="http://code.jquery.com/jquery-latest.js"></script>
         <script src="assets/js/header.js" type="text/javascript"></script>
-	<title>Muro de las lamentaciones</title>
-</head>
-<body>
-<header>
-<div class="wrapper">
-	<div class="logo">Muro de las lamentaciones</div>
-</div>
-</header>
-<section class="contenido wrapper">
-</section>
-<footer>
-		<div class="copyrigth wrapper">
-			<p align="center">Copyright &copy; 2015 |Muro de las lamentaciones</p>
-		</div>
-	</footer>
-</body>
+        <title>Muro de las lamentaciones</title>
+    </head>
+    <body>
+        <header>
+            <div class="wrapper">
+                <div class="logo">Muro de las lamentaciones</div>
+            </div>
+        </header>
+        <section class="contenido wrapper">
+        </section>
+        <footer>
+            <div class="copyrigth wrapper">
+                <p align="center">Copyright &copy; 2015 |Muro de las lamentaciones</p>
+            </div>
+        </footer>
+    </body>
 </html>
