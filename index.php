5f3c2a8fcddabbaf06334f7fca8092244b130bf8
Sebastian <>
2016-04-05 20:51:26 -0500

    Añado HTML base

--- index.php
+++ index.php
@@ -1,10 +1,24 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>OpenWall</title>
-	<meta charset="utf-8">
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="css/estilos.css" type="text/css" />
+	<script src="http://code.jquery.com/jquery-latest.js"></script>
+	<script src="js/header.js" ></script>
+	<title>Muro de las lamentaciones</title>
 </head>
 <body>
-
+<header>
+<div class="wrapper">
+	<div class="logo">Muro de las lamentaciones</div>
+</div>
+</header>
+<section class="contenido wrapper">
+</section>
+<footer>
+		<div class="copyrigth wrapper">
+			<p align="center">Copyright &copy; 2015 |Muro de las lamentaciones</p>
+		</div>
+	</footer>
 </body>
 </html>
