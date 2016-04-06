4ba8a488d4a1019ad588a55155e36dd1b0e89c98
Cesar Zabala <cesarzabala@gmail.com>
2016-04-05 21:18:15 -0500

    test de Connection

---
+++ test/testConnection.php
@@ -0,0 +1,19 @@
+<?php 
+include("../dao/ConexionBD.php");
+include("../bo/ClsPublication.php");
+
+$conex = new ConexionBD();
+
+echo "<pre>";
+print_r($conex->getAllPublications());
+echo "</pre>";
+
+//Prueba de registro
+$publication = new Publication("","Mensaje de prueba", 2, "2016-03-15");
+$response = $conex->insertPublication($publication);
+
+echo "<pre>";
+print_r($response);
+echo "</pre>";
+
+ ?>
