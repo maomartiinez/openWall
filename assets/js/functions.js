92bcb1c529f8cf2637d80db9be07c7c48b593e87
cricalonos <cricalonos@gmail.com>
2016-04-06 21:24:25 -0500

    Se agregó la función de traer todos los comentarios

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,4 +1,19 @@
-$(document).ready(function(){
-
-	
+$(document).ready(function () {
+    cargarTodo();
 });
+
+function cargarTodo() {
+    $.ajax({
+        type: "POST",
+        url: "bo/ListarPubBO.php",
+        success: function (data) {
+            obj = JSON.parse(data);
+            var html = "";
+            for (i = 0; i < obj.length; i++) {
+                html += "<div>" + obj[i].text_content + "</div>";
+            }
+            $('#muro').append(html);
+        },
+    });
+    return false;
+}
